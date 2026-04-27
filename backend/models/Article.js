5d6cd6f7b4a4a23ba0cacaf09ae36db676cb163b
Rajeev <rajeevrayal2864@gmail.com>
2026-04-27 15:42:17 +0530

    switched to mongodb fully

---
+++ backend/models/Article.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const articleSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+  cover_image: String,
+  video: String
+}, { timestamps: true });
+
+module.exports = mongoose.model('Article', articleSchema);
